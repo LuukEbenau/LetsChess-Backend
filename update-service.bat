0a3821c840d0ad208e84750103fc3ef8b2f728ed
LuukEbenau <l.ebenau@hotmail.com>
2021-05-26 00:47:45 +0200

    added stuff

--- update-service.bat
+++ update-service.bat
@@ -2,4 +2,3 @@
 ECHO Lets update the service!
 docker build . -t sacation/letschess-backend
 docker push sacation/letschess-backend
-kubectl delete pods -l letschess.service=letschessbackend -n letschess
